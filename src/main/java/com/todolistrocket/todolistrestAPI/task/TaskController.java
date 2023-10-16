0b92ff9967bd119c49d33a9f40a7eeb3f99790bb
AlexadreNoguez <alexandrenoguez_rs@yahoo.com.br>
2023-10-15 22:16:07 -0300

    feat: added put method to tasks

--- src/main/java/com/todolistrocket/todolistrestAPI/task/TaskController.java
+++ src/main/java/com/todolistrocket/todolistrestAPI/task/TaskController.java
@@ -1,11 +1,8 @@
 package com.todolistrocket.todolistrestAPI.task;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,69 +12,40 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.todolistrocket.todolistrestAPI.utils.Utils;
-
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
 
-    @Autowired
-    private ITaskRepository taskRepository;
+    private final TaskService taskService;
 
     @PostMapping
     public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
-        var idUser = request.getAttribute("idUser");
-        taskModel.setIdUser((UUID) idUser);
-
-        var currentDate = LocalDateTime.now();
-        // 10/11/2023 - Current
-        // 10/10/2023 - startAt
-        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("A data de início / data de término deve ser maior do que a data atual");
-        }
-
-        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("A data de início deve ser menor do que a data de término");
-        }
-
-        var task = this.taskRepository.save(taskModel);
-        return ResponseEntity.status(HttpStatus.OK).body(task);
-
+        var idUser = (UUID) request.getAttribute("idUser");
+        TaskModel createdTask = taskService.createTask(taskModel, idUser);
+        return ResponseEntity.ok(createdTask);
     }
 
     @GetMapping
     public List<TaskModel> list(HttpServletRequest request) {
-        var idUser = request.getAttribute("idUser");
-        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        var idUser = (UUID) request.getAttribute("idUser");
+        List<TaskModel> tasks = taskService.getTasksByUserId(idUser);
         return tasks;
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<TaskModel> update(@RequestBody TaskModel taskModel, @PathVariable("id") UUID id,
-            HttpServletRequest request) {
-        var task = this.taskRepository.findById(id).orElse(null);
-
-        if (task == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(null); // Você pode retornar null como um marcador de erro, ou criar um objeto
-                                 // específico para isso
+            HttpServletRequest request) throws Exception {
+        var idUser = (UUID) request.getAttribute("idUser");
+        TaskModel updatedTask = taskService.updateTask(id, taskModel, idUser);
+        if (updatedTask != null) {
+            return ResponseEntity.ok(updatedTask);
+        } else {
+            return ResponseEntity.badRequest().body(null); // Tratar erro apropriadamente
         }
-
-        var idUser = request.getAttribute("idUser");
-
-        if (!task.getIdUser().equals(idUser)) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(null); // Mesma observação aqui
-        }
-
-        Utils.copyNonNullProperties(taskModel, task);
-
-        var taskUpdated = this.taskRepository.save(task);
-        return ResponseEntity.ok().body(this.taskRepository.save(taskUpdated));
     }
 
 }
